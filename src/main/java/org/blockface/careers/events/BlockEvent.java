d1cd4e32e08b61567924c57cc25c0bf21c562115
Joshua Lacey <thedragon89@sbcglobal.net>
2011-10-09 16:11:40 -0500

    Finished Farmer. Fixed some bugs.

--- src/main/java/org/blockface/careers/events/BlockEvent.java
+++ src/main/java/org/blockface/careers/events/BlockEvent.java
@@ -21,17 +21,17 @@ public class BlockEvent extends BlockListener{
        public void onBlockBreak(BlockBreakEvent event){
            int bid = event.getBlock().getTypeId();
            Player player = event.getPlayer();
-             if((bid <= 16 && bid >= 12) || bid == 21 || bid == 56 || bid == 73 || bid == 74){
+             if((bid <= 16 && bid >= 14) || bid == 21 || bid == 56 || bid == 73 || bid == 74){
                 if(CareersEvents.DoubleDrop(player)){
                     //could not figure out how to actually make the block drop twice. So I just added the item to the inventory.
                     if(bid == 73 || bid == 74)
-                         player.getInventory().addItem(new ItemStack(Material.REDSTONE, 1));
+                         player.getInventory().addItem(new ItemStack(Material.REDSTONE, 4));
 
                     else if(bid == 56)
                         player.getInventory().addItem(new ItemStack(Material.DIAMOND, 1));
 
                     else if(bid == 21)
-                        player.getInventory().addItem(new ItemStack(Material.LAPIS_ORE, 1));
+                        player.getInventory().addItem(new ItemStack(Material.INK_SACK, 4, (short) 0, (byte) 4));
 
                     else if(bid == 16)
                         player.getInventory().addItem(new ItemStack(Material.COAL, 1));
@@ -41,5 +41,17 @@ public class BlockEvent extends BlockListener{
                 }
              }
        }
+
+    public void onBlockPlace(BlockPlaceEvent event){
+        if(event.getBlockPlaced().getType() == Material.IRON_ORE || event.getBlockPlaced().getType() == Material.GOLD_ORE){
+
+            event.getBlock().setType(Material.AIR);
+            event.getPlayer().getItemInHand().setAmount(event.getPlayer().getItemInHand().getAmount() + 1);
+
+        }
+
+
+    }
+
 
 }
